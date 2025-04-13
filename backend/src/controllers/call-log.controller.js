50cecdc0570bc5d57a0fcb9c6d0a2197b63482d3
creator-dhruv <dhruvvarshney27@gmail.com>
2025-04-14 02:16:51 +0530

    .

--- backend/src/controllers/call-log.controller.js
+++ backend/src/controllers/call-log.controller.js
@@ -1,79 +1,113 @@
-import { Caller } from "../models/emergency-call-log.model.js"
+import { Caller } from "../models/emergency-call-log.model.js";
+import twilio from "twilio";
+import { User } from "../models/user.model.js";
 
-const addCaller = async (req,res) => {
-    try {
-        const {callerInfo} = req.body
-        const user = req.user
-    
-        const existedCallLog = await Caller.aggregate([
-            {$match: {
-                "callerDetail" : callerInfo
-            }}
-        ])
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
 
-        // console.log(existedCallLog)
-    
-        if (existedCallLog[0]) {
-    
-            return res.json({
-                message: "Caller detail already existed.",
-                success: true
-            })
-        }
-    
-        const callLog = await Caller.create({
-            userID: user._id,
-            callerDetail: callerInfo
-        })
-    
-        const createdCallLog = await Caller.findById(callLog._id)
-    
-        return res.json({
-            message: createdCallLog,
-            success: true
-        })
-    } catch (error) {
-        console.log(error)
-        return
+const createCall = async (req, res) => {
+  const userID = req.user._id;
+
+  const user = await User.findById(userID);
+
+  const callerDetail = await Caller.find({ userID });
+  console.log(callerDetail[0].number, callerDetail);
+
+  const client = twilio(accountSid, authToken);
+
+  for (let index = 0; index < callerDetail.length; index++) {
+    const element = callerDetail[index];
+    const call = await client.calls.create({
+      from: process.env.TWILIO_PHONE_NUMBER,
+      to: `+91${element.callerDetail.number}`,
+      twiml: `
+      <Response>
+      <Pause />
+      <Say language="hi-IN" voice="Kajal-Neural" > Hi <Pause/> ${element.callerDetail.name} , <Pause/> your <Pause/> ${element.callerDetail.relation} <Pause/> , ${user.fullName} <Pause/> need your help. Either you <Pause/> call her or <Pause/> track their location.</Say>
+      <Pause/> 
+      </Response>`,
+    });
+  }
+
+  //   console.log(call.sid);
+
+  return res.json({
+    message: "All calls ended.",
+    success: true,
+  });
+};
+
+const addCaller = async (req, res) => {
+  try {
+    const { callerInfo } = req.body;
+    const user = req.user;
+
+    const existedCallLog = await Caller.aggregate([
+      {
+        $match: {
+          callerDetail: callerInfo,
+        },
+      },
+    ]);
+
+    // console.log(existedCallLog)
+
+    if (existedCallLog[0]) {
+      return res.json({
+        message: "Caller detail already existed.",
+        success: true,
+      });
     }
 
-}
+    const callLog = await Caller.create({
+      userID: user._id,
+      callerDetail: callerInfo,
+    });
 
-
-const deleteCaller = async (req,res) => {
-    const {id} = req.body
-    const user = req.user
-
-    const caller = await Caller.findById(id)
-    // console.log(user._id , caller.userID , caller)
-
-    if (user._id.toString() !== caller.userID.toString()) {
-        return res.json({
-            message: "Unauthorized request.",
-            success: false
-        })
-    }
-
-    await Caller.findByIdAndDelete(id)
+    const createdCallLog = await Caller.findById(callLog._id);
 
     return res.json({
-        message: "Caller info deleted successfully.",
-        success: true
-    })
-}
+      message: createdCallLog,
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+    return;
+  }
+};
 
+const deleteCaller = async (req, res) => {
+  const { id } = req.body;
+  const user = req.user;
 
-const getCaller = async (req,res) => {
-    const userID = req.user._id
+  const caller = await Caller.findById(id);
+  // console.log(user._id , caller.userID , caller)
 
-    const callerDetail = await Caller.find({userID})
-
+  if (user._id.toString() !== caller.userID.toString()) {
     return res.json({
-        message: "Data fetched successfully.",
-        data: callerDetail,
-        success: true
-    })
-}
+      message: "Unauthorized request.",
+      success: false,
+    });
+  }
 
+  await Caller.findByIdAndDelete(id);
 
-export {addCaller, deleteCaller, getCaller}
+  return res.json({
+    message: "Caller info deleted successfully.",
+    success: true,
+  });
+};
+
+const getCaller = async (req, res) => {
+  const userID = req.user._id;
+
+  const callerDetail = await Caller.find({ userID });
+
+  return res.json({
+    message: "Data fetched successfully.",
+    data: callerDetail,
+    success: true,
+  });
+};
+
+export { createCall, addCaller, deleteCaller, getCaller };
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,55 +1,53 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-import cors from "cors"
-import dotenv from "dotenv"
+import cors from "cors";
+import dotenv from "dotenv";
 import dbconnect from "./utility/db-connection.js";
 
-
 dotenv.config({
-    path: "./.env"
-})
-const app = express()
+  path: "./.env",
+});
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser())
-app.use(cors({
-    origin: "*" ,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
-}))
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
-dbconnect()
+dbconnect();
 
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`http://localhost:${process.env.PORT}`);
+});
 
-app.listen(process.env.PORT || 3000,() => {
-    console.log(`http://localhost:${process.env.PORT}`)
-})
-
-app.get("/", (req,res) => {
-    res.send({
-        message: "i am connected"
-    })
-})
+app.get("/", (req, res) => {
+  res.send({
+    message: "i am connected",
+  });
+});
 
 // All Controllers import
 import { googleLogin } from "./controllers/user.controller.js";
-import { addCaller, deleteCaller, getCaller } from "./controllers/call-log.controller.js";
+import {
+  addCaller,
+  deleteCaller,
+  getCaller,
+  createCall,
+} from "./controllers/call-log.controller.js";
 import { verifyUserJWT } from "./middlewares/auth.js";
 
-
-
-
 // All Routes
-app.post("/api/user/googleLogin",googleLogin)
-app.post("/api/caller/add", verifyUserJWT, addCaller)
-app.delete("/api/caller/delete", verifyUserJWT, deleteCaller)
-app.get("/api/caller/get", verifyUserJWT, getCaller)
+app.post("/api/user/googleLogin", googleLogin);
+app.post("/api/caller/add", verifyUserJWT, addCaller);
+app.delete("/api/caller/delete", verifyUserJWT, deleteCaller);
+app.get("/api/caller/get", verifyUserJWT, getCaller);
+app.get("/api/caller/create", verifyUserJWT, createCall);
 
-
-
-
-
-
-export {app}
+export { app };
